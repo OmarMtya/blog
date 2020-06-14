670099fb713b25a106180b7700dc6070c25e8ce5
OmarMtya <luis.montoya.j@gmail.com>
2020-06-14 02:31:56 -0600

    Colores de cartas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { PortadaComponent } from './componentes/portada/portada.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostComponent } from './componentes/post/post.component';
 
 
 const routes: Routes = [
+  { path: '', component: PortadaComponent },
   { path: 'post', component: PostComponent },
-  { path: '', component: PostComponent },
   { path: '**', component: PostComponent },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PostComponent } from './componentes/post/post.component';
 import { HeaderComponent } from './componentes/shared/header/header.component';
 import { ComentarioComponent } from './componentes/post/comentario/comentario.component';
+import { PortadaComponent } from './componentes/portada/portada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostComponent,
     HeaderComponent,
-    ComentarioComponent
+    ComentarioComponent,
+    PortadaComponent
   ],
   imports: [
     BrowserModule,
