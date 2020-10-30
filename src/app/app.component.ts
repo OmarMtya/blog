ce84ab5207f9090b11435e17e2bb343c4e5ee135
Omar Montoya <luis.montoya.j@gmail.com>
2020-10-30 03:40:25 -0600

    Animaciones de entrada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos/dist/aos.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'omarmtya';
+
+  constructor(){
+    AOS.init();
+  }
 }
