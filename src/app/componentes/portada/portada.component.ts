e68b1017d0c01434407e78fa20409033253378f8
Omar Montoya <luis.montoya.j@gmail.com>
2020-10-30 05:53:17 -0600

    Favicon y cambios menores

--- src/app/componentes/portada/portada.component.ts
+++ src/app/componentes/portada/portada.component.ts
@@ -1,26 +1,33 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit, Input } from '@angular/core';
 import { Proyecto } from 'src/app/models/proyectos.model';
 import { ProyectosService } from '../../servicios/proyectos.service';
 @Component({
-  selector: 'app-portada',
-  templateUrl: './portada.component.html',
-  styleUrls: ['./portada.component.scss'],
+  selector: "app-portada",
+  templateUrl: "./portada.component.html",
+  styleUrls: ["./portada.component.scss"],
 })
 export class PortadaComponent implements OnInit {
   proyectoSeleccionado: Proyecto = null;
   proyectos: Proyecto[];
 
-  constructor(private proyectosService: ProyectosService) {}
+  constructor(
+    private proyectosService: ProyectosService,
+    private viewportScroller: ViewportScroller) {}
 
   ngOnInit() {
     this.proyectos = this.proyectosService.getProyectos();
   }
 
-  proyectoSeleccionadoEvent(proyecto: Proyecto){
+  proyectoSeleccionadoEvent(proyecto: Proyecto) {
     this.proyectoSeleccionado = proyecto;
   }
 
-  deseleccionarProyecto(){
+  scrollTo(elementId: string): void {
+    this.viewportScroller.scrollToAnchor(elementId);
+  }
+
+  deseleccionarProyecto() {
     this.proyectoSeleccionado = null;
   }
 }
