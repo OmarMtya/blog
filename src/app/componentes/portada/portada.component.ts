62d9718687ce2a8fbfeaeca13735dcd5b6e9242d
Omar Montoya <luis.montoya.j@gmail.com>
2020-10-30 00:15:07 -0600

    Antes del update

--- src/app/componentes/portada/portada.component.ts
+++ src/app/componentes/portada/portada.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-
 @Component({
   selector: 'app-portada',
   templateUrl: './portada.component.html',
@@ -10,6 +9,16 @@ export class PortadaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    console.log("ENTRO");
+    var videoElement = document.getElementById("videoElement");
+    // @ts-ignore
+    var flvPlayer = flvjs.createPlayer({
+      type: "flv",
+      url: "ws://localhost:8000/live/123.flv",
+    });
+    flvPlayer.attachMediaElement(videoElement);
+    flvPlayer.load();
+    flvPlayer.play();
   }
 
 }
